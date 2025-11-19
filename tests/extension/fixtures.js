99d7cfcaf9bb79016caef1222d408e0e29661e0f
ChunkyNosher <67388561+ChunkyNosher@users.noreply.github.com>
2025-11-19 16:39:18 -0500

    Add Playwright testing fixtures for Chrome extension
    This file sets up testing fixtures for Playwright, allowing for extension testing with a pre-installed Chrome extension.

---
+++ tests/extension/fixtures.js
@@ -0,0 +1,36 @@
+// tests/extension/fixtures.js
+const { test as base, chromium } = require('@playwright/test');
+const path = require('path');
+
+/**
+ * Extension testing fixture
+ * Loads Chrome with extension pre-installed
+ */
+exports.test = base.extend({
+  context: async ({ }, use) => {
+    const pathToExtension = path.join(__dirname, '../../dist');
+    
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    });
+    
+    await use(context);
+    await context.close();
+  },
+  
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent('serviceworker');
+
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+
+exports.expect = base.expect;
